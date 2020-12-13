330c7ac5a0a86b67fba43ddd4b6ea70328bf0b78
HardRain <nikolaimihail@gmail.com>
2020-12-14 01:35:54 +0200

    Updates

---
+++ precache-manifest.e0252a162316784b015d454de81f2148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c756f12ce21882954f5a9d75164a13",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "221a025dd0cb657ac8f5",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.cf4eb493.chunk.css"
+  },
+  {
+    "revision": "02b725cd8a7e5ada4048",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.b96a9e4e.chunk.js"
+  },
+  {
+    "revision": "36335cb7ef53d23e10deaa53983c3f0f",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.b96a9e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221a025dd0cb657ac8f5",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.61a64cd7.chunk.js"
+  },
+  {
+    "revision": "3f441844d3ba7dba1f3a",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.61bf2916.js"
+  }
+]);
