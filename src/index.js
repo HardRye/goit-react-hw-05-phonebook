f813d1f77215b59d8d55dd147c115db164ff3835
HardRain <nikolaimihail@gmail.com>
2020-12-14 22:38:25 +0200

    test netlify deploy 3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/App';
+// import App from './components/App';
 import store from './redux/store';
 
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <h1>Hello world</h1>
+    {/* <App /> */}
   </Provider>,
   document.querySelector('#root'),
 );
